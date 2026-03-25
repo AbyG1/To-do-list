679b36272cc2131e0fcd44a79142c14ba37b2686
Aby <aby@zoondia.net>
2026-03-25 16:52:36 +0530

    handleAppstate

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function createToDoList(name, desc, note, date, importance, status) {
+function createTodoList(name, desc, note, date, importance, status) {
     
     const id = uuidv4();
     let title = name;
@@ -41,42 +41,51 @@ const createProject = (name) => {
         const projectName = name;
         const projectId = uuidv4(); 
         const createdDate = getDate();
-        let toDos = [];
+        let todos = [];
 
     
 
 
 
-    const getToDos = () => [...toDos];
+    const getTodos = () => [...todos];
 
-    const addToDo = (Todo) => toDos.push(Todo);
+    const addTodo = (todo) => todos.push(todo);
 
-    const deleteTodoList = (id) => {
-        toDos = toDos.filter(item => item.id !== id)
+    const deleteTodoList = (todoId) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todos.splice(toDoIndex,1);
+        console.log(todos);
+    };
+
+    const updateTodoStatus = (todoId,status) => {
+         const toDoIndex = todos.findIndex(todo => todo.id === id);
+         todoId[toDoIndex].isCompleted = status;
+         
+    };
+
+    const updateTodoPriority = (todoId, priority) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todoId[toDoIndex].priority = priority;
+    };
+
+    const editTodoTitle = (todoId,title) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todoId[toDoIndex].title = title;
     }
 
-    const markTodoComplete = (id) => {
-
+    const editTodoDescription = (todoId,desc) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todoId[toDoIndex].description = desc;
     }
 
-    const updateTodoPriority = (id) => {
-
+    const editTodoNotes = (todoId,notes) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todoId[toDoIndex].notes = notes;
     }
 
-    const editTodoTitle = (id) => {
-
-    }
-
-    const editTodoDescription = (id) => {
-
-    }
-
-    const editTodoNotes = (id) => {
-
-    }
-
-    const changeTododueDate = (id) => {
-
+    const changeTodoDueDate = (todoId) => {
+        const toDoIndex = todos.findIndex(todo => todo.id === id);
+        todoId[toDoIndex].dueDate = dueDate;
     }
 
 
@@ -85,15 +94,15 @@ const createProject = (name) => {
         projectName,
         projectId,
         createdDate,
-        addToDo,
+        addTodo,
         deleteTodoList,
-        getToDos,
-        markTodoComplete,
+        getTodos,
+        updateTodoStatus,
         updateTodoPriority,
         editTodoTitle,
         editTodoDescription,
         editTodoNotes,
-        changeTododueDate
+        changeTodoDueDate
     }
 
 
@@ -101,13 +110,29 @@ const createProject = (name) => {
 
 
 
-function assignTodotoProject(projectId){
-    const toDo = createToDoList(title, description, notes, dueDate, priority,isCompleted,)
-    handleProject.addToDo(toDo);
 
-}
+const handleAppstate = () =>  {
+    
+    const appState = [];
+
+    const addProject = (projectName) => {
+        const newProject = createProject(projectName);
+        appState.push(newProject);
+
+    };
+
+    const addTodoToProject = (projectId, todoId) => {
+        const index = appState.findIndex(project => project.projectId === projectId);
+        appState[index]
+    };
+
+    const deleteProject = (projectId) => {
+        const index =  appState.findIndex(project => project.projectId === projectId);
+        appState.slice(index,1);
+        console.log(appState);
+
+    };
 
 
-const handleAppstate() => {
 
 }
